46e94edd4d3f45a195e419e1e8dfedcee24f8d51
Soham Sahare <sohamsahare@gmail.com>
2018-09-07 15:21:11 +0530

    added relevant code that passed the test cases

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -6,8 +6,31 @@ namespace HoloalphabeticSentence
     public class HoloalphabeticSentence
     {
         // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
-        public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
+        public static bool IsHoloalphabeticSentence(string sentence, string alphabets = "abcdefghijklmnopqrstuvwxyz")
         {
+            // change to lowercase
+            sentence = sentence.ToLower();
+            // array to count occurence of each alphabet
+            int[] characterSet = new int[26];
+            int asciiValue = -1;
+            // if sum is 26 then it's an holoalphabetic sentence
+            int sum = 0;
+            const int characterOffset = 97;
+            // checking for each alphabet
+            for(int i = 0; i < sentence.Length; i++){
+                asciiValue = sentence[i] - characterOffset;
+                if(asciiValue >= 0){
+                    characterSet[asciiValue] = 1;
+                }
+            }
+            // summing all output
+            for(int i = 0; i < 26; i++){
+                sum += characterSet[i];
+            }
+            // final check
+            if(sum == 26){
+                return true;
+            }
             return false;
         }
     }
